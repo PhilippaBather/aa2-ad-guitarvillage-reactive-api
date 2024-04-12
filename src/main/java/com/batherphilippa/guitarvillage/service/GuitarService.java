28f48ab785d561c692969cd36e4d4efe7d3c38c7
pabather <p.a.bather1@gmail.com>
2024-04-12 14:40:38 +0200

    updated

--- src/main/java/com/batherphilippa/guitarvillage/service/GuitarService.java
+++ src/main/java/com/batherphilippa/guitarvillage/service/GuitarService.java
@@ -1,6 +1,7 @@
 package com.batherphilippa.guitarvillage.service;
 
 import com.batherphilippa.guitarvillage.domain.Guitar;
+import com.batherphilippa.guitarvillage.domain.GuitarDTOIn;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
@@ -9,7 +10,7 @@ public interface GuitarService {
     Flux<Guitar> findAll();
     Mono<Guitar> findById(String id);
     Mono<Guitar> save(Guitar guitar);
-    Mono<Guitar> updateById(Mono<Guitar> guitar);
+    Mono<Guitar> updateById(Mono<GuitarDTOIn> guitar, String id);
     Mono<Void> deleteById(String id);
 
 }
