9708b52e3dfa5d03101306e9b602d1ed899bd763
pabather <p.a.bather1@gmail.com>
2024-05-03 15:50:53 +0200

    added order router

---
+++ src/main/java/com/batherphilippa/guitarvillage/router/OrderRouter.java
@@ -0,0 +1,25 @@
+package com.batherphilippa.guitarvillage.router;
+
+import com.batherphilippa.guitarvillage.handler.OrderHandler;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
+import org.springframework.web.reactive.function.server.RouterFunction;
+import org.springframework.web.reactive.function.server.RouterFunctions;
+import org.springframework.web.reactive.function.server.ServerResponse;
+
+import static org.springframework.web.reactive.function.server.RequestPredicates.*;
+
+@Configuration
+public class OrderRouter {
+
+    @Bean
+    public RouterFunction<ServerResponse> guitarsRoute(OrderHandler orderHandler) {
+        return RouterFunctions
+                .route(GET("/orders").and(accept(MediaType.APPLICATION_JSON)), orderHandler::getAllOrders)
+                .andRoute(POST("/orders").and(accept(MediaType.APPLICATION_JSON)), orderHandler::createOrder)
+                .andRoute(GET("/orders/{orderId}").and(accept(MediaType.APPLICATION_JSON)), orderHandler::getOrderById)
+                .andRoute(PUT("/orders/{orderId}").and(accept(MediaType.APPLICATION_JSON)), orderHandler::updateOrderById)
+                .andRoute(DELETE("/orders/{orderId}").and(accept(MediaType.APPLICATION_JSON)), orderHandler::deleteOrderById);
+    }
+}
